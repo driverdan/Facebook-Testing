9344a577cc52398073429aabf8a1d76b185533dc
Dan DeFelippi <dan@driverdan.com>
2012-01-25 17:57:39 -0600

    Tests for getaccesstoken

--- tests/getaccesstoken.js
+++ tests/getaccesstoken.js
@@ -5,6 +5,23 @@ var config = require('../config').config;
 
 var fbapp = new FbApp(config);
 
+// Clear the access token to be sure it's set later
+fbapp.appAccessToken = "";
+
 fbapp.getAccessToken(function(token) {
-  console.log("Token: " + token);
+  console.log("Response: " + token);
+
+  var tokenSplit = token.split("=");
+
+  if (tokenSplit.length >= 2 && tokenSplit[0] === "access_token") {
+    console.log("Token fetched successfully");
+
+    if (fbapp.appAccessToken === token) {
+      console.log("fbapp.appAccessToken set correctly");
+    } else {
+      console.log("fbapp.appAccessToken not set correctly: " + fbapp.appAccessToken);
+    }
+  } else {
+    console.log("Token fetch FAILED");
+  }
 });
