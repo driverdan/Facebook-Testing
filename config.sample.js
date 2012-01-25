a02b9e692ed324c2f2af095e2b44f7032b2564f6
Dan DeFelippi <dan@driverdan.com>
2012-01-24 18:56:17 -0600

    Add sample config

---
+++ config.sample.js
@@ -0,0 +1,15 @@
+exports.config = {
+  // Path to your app under apps.facebook.com
+  graphPrefix: '',
+  // App ID number
+  appId: '',
+  // App secret string
+  appSecret: '',
+  // App access token in format access_token=xxxxxxxxx|aaaaaaaaaaaaaaaaaaaa
+  appAccessToken: '',
+
+  // Optional array of users to ignore for operations such as friending
+  ignoredUsers: [],
+  // Optional array of test user data objects in same format as API response
+  testUsers: []
+};
